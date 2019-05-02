f6ab5928e0d4b1b1a9db3d06c5520ae03c9f2df2
Joshua Samuel <tweeterfriend@gmail.com>
2019-05-02 11:59:00 -0700

    naive solution

--- solver.py
+++ solver.py
@@ -1,4 +1,15 @@
-# Put your solution here.
-
 def solve(client):
-    pass
+    client.end()
+    client.start()
+
+    MST_tree = nx.minimum_spanning_tree(client.G)
+
+    preorder_list = list(nx.dfs_preorder_nodes(MST_tree, source=client.home))[::-1]
+
+
+    for v in range(len(preorder_list) - 1):
+    	for v_e in range(v, len(preorder_list)):
+    		if(preorder_list[v], preorder_list[v_e]) in MST_tree.edges():
+    			client.remote(preorder_list[v],preorder_list[v_e])
+
+    client.end()
