1b6a457f128802ce0bd5d2ba686901f9652a04d2
Ramsey Mardini <ramseymardini@berkeley.edu>
2019-05-02 18:49:26 -0700

    Started implementing 4b

--- solver.py
+++ solver.py
@@ -4,16 +4,29 @@ import random
 def solve(client):
     client.end()
     client.start()
-    
-    MST_tree = nx.minimum_spanning_tree(client.G)
 
-    preorder_list = list(nx.dfs_preorder_nodes(MST_tree, source=client.home))[::-1]
+    if (client.v == client.bots):
+        run_naive_MST(client)
 
-    for v in range(len(preorder_list) - 1):
-    	for v_e in range(v + 1, len(preorder_list)):
-    		if (preorder_list[v], preorder_list[v_e]) in MST_tree.edges():
-    			client.remote(preorder_list[v],preorder_list[v_e])
-
+    run_naive_dijk(client)
 
 
     client.end()
+
+#This one just finds the MST and remotes across the MST.
+def run_naive_MST(client):
+    MST_tree = nx.minimum_spanning_tree(client.G)
+
+    postorder_list = list(nx.dfs_postorder_nodes(MST_tree, source=client.home))
+
+    for v in range(len(postorder_list) - 1):
+    	for v_e in range(v + 1, len(postorder_list)):
+    		if (postorder_list[v], postorder_list[v_e]) in MST_tree.edges():
+    			client.remote(postorder_list[v],postorder_list[v_e])
+
+#This one is 4b on design doc
+def run_naive_dijk(client):
+    #sorted_edge_list = print(client.G.edges())
+    print(client.G.edgelist)
+    #for edge in sorted_edge_list:
+    #    print(edge)
