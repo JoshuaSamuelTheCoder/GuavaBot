3e52309123f638f61e7564c2dfaea11950fbbc7c
Brian Yin <brianyin@berkeley.edu>
2019-05-02 20:21:38 -0700

    find all bots by iterating through edges

--- solver.py
+++ solver.py
@@ -13,7 +13,7 @@ def solve(client):
 
     client.end()
 
-#This one just finds the MST and remotes across the MST.
+# Find the MST and remotes across the MST.
 def run_naive_MST(client):
     MST_tree = nx.minimum_spanning_tree(client.G)
 
@@ -24,9 +24,27 @@ def run_naive_MST(client):
     		if (postorder_list[v], postorder_list[v_e]) in MST_tree.edges():
     			client.remote(postorder_list[v],postorder_list[v_e])
 
-#This one is 4b on design doc
+# This one is 4b on design doc
 def run_naive_dijk(client):
-    #sorted_edge_list = print(client.G.edges())
-    print(client.G.edgelist)
-    #for edge in sorted_edge_list:
-    #    print(edge)
+
+    seenNodes = dict((node, False) for node in client.G.nodes) # dictionary with (int node, boolean) pairs
+    botsAtNode = dict((node, -1) for node in client.G.nodes) # number of bots at each node (-1 for unseen nodes)
+    edge_list = []
+
+    for (vert1, vert2, weight) in client.G.edges.data('weight',default=1): # get triples of the form (int first vertex, int second vertex, int edge weight)
+        edge_list.append((vert1, vert2, weight))
+
+    sorted_edge_list = sorted(edge_list, key=lambda x:x[2]) # sort triples by edge weight
+
+    for edge in sorted_edge_list:   # iterate through sorted edges
+        if (False in seenNodes.values()): # if we still have unseen nodes
+            vert1, vert2 = edge[0], edge[1]
+            seenNodes[vert1], seenNodes[vert2] = True, True
+
+            client.remote(vert1, vert2)
+            botsAtNode[vert1] = client.remote(vert2, vert1)
+            botsAtNode[vert2] = 0
+
+    for key in botsAtNode: # print all vertices with bots
+        if botsAtNode[key] > 0:
+            print(key,botsAtNode[key])
