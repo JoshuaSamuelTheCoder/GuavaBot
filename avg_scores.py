f901d999ea69a0fd4d188cd0da6065794a838a20
Joshua Samuel <tweeterfriend@gmail.com>
2019-05-06 15:43:00 -0700

    adapted script to cancer terminal output and it's 3 different outputs

--- avg_scores.py
+++ avg_scores.py
@@ -4,62 +4,60 @@ import re
 
 runs = input("Enter number of runs to try: ")
 num_scouts = 0
-try:
-    r = int(runs)
-    scores10 = []
-    scores20 = []
-    scores40 = []
-    for i in range(1, r + 1):
-        output = check_output(["python","client.py","--solver","solver"])
-
-        a = output[221:229]
-
-        print(a)
-
-        print(a[0:2])
-        num_scouts = int(a[0:2])
-        print(num_scouts)
-        score = float(re.sub(r'[^\d.]+', "", str.split(str(output), "Score: ")[1]))
-        print("Attempt " + str(i) + ": " + str(score))
-
-        if num_scouts == 40:
-            scores40.append(score)
-        elif num_scouts == 20:
-            scores20.append(score)
-        elif num_scouts == 10:
-            scores10.append(score)
-
-    print("Average Score for " + str(r) + " runs: " + "\n")
 
 
-    if len(scores10) > 0:
-        avg10 = stats.mean(scores10)
-        print("Average Score for 10 students " + str(avg10) + "\n")
-    if len(scores20) > 0:
-        avg20 = stats.mean(scores20)
-        print("Average Score for 20 students " + str(avg20) + "\n")
-    if len(scores40) > 0:
-        avg40 = stats.mean(scores40)
-        print("Average Score for 40 students " + str(avg40) + "\n")
+r = int(runs)
+scores10 = []
+scores20 = []
+scores40 = []
+for i in range(1, r + 1):
+    output = check_output(["python","client.py","--solver","solver"])
 
-    exit()
+    a = output[221:229]
 
-except ValueError:
-   print("Error: Not an int")
+    print(a)
 
-   print("Average Score for " + str(r) + " runs: " + "\n")
+    print(a[0:2])
+ 
+
+    is_int = False
+    try:
+        num = int(a[0:2])
+        is_int = True
+    except ValueError:
+        is_int = False
+        
+    
+    if(is_int == False):
+        continue
+    num_scouts = int(a[0:2])
+    print(num_scouts)
+    score = float(re.sub(r'[^\d.]+', "", str.split(str(output), "Score: ")[1]))
+    print("Attempt " + str(i) + ": " + str(score))
+
+    if num_scouts == 40:
+        scores40.append(score)
+    elif num_scouts == 20:
+        scores20.append(score)
+    elif num_scouts == 10:
+        scores10.append(score)
+
+print("Average Score for " + str(r) + " runs: " + "\n")
 
 
-   if len(scores10) > 0:
-        avg10 = stats.mean(scores10)
-        print("Average Score for 10 students " + str(avg10) + "\n")
-   if len(scores20) > 0:
-        avg20 = stats.mean(scores20)
-        print("Average Score for 20 students " + str(avg20) + "\n")
-   if len(scores40) > 0:
-        avg40 = stats.mean(scores40)
-        print("Average Score for 40 students " + str(avg40) + "\n")
+if len(scores10) > 0:
+    avg10 = stats.mean(scores10)
+    print("Average Score for 10 students " + str(avg10) + "\n")
+if len(scores20) > 0:
+    avg20 = stats.mean(scores20)
+    print("Average Score for 20 students " + str(avg20) + "\n")
+if len(scores40) > 0:
+    avg40 = stats.mean(scores40)
+    print("Average Score for 40 students " + str(avg40) + "\n")
+
+exit()
 
 
 
-   exit()
+
+  
